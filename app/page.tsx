4ebfd6ea608c941c2b0b60c0b306b8e08508590b
Jevis Kafle <jeviskafle@gmail.com>
2026-04-20 20:14:44 +0545

    Lobby page for chat app and dynamic routing for chat rooms

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,35 @@
-import Image from "next/image";
+import { useState } from "react";
+import { useRouter } from "next/router";
+
+
 
 export default function Home() {
+  const router = useRouter();
+  const [username, setUsername] = useState("");
+  const [room, setRoom] = useState("general");
+
+  const join = () => {
+    if(!username.trim()) return;
+    localStorage.setItem("username", username);
+    router.push(`/chat/${room}`);
+  }
+
   return (
     <div>
-      hello
+      <h1>Chat Lobby</h1>
+      <input
+        type="text"
+        placeholder="Enter your username"
+        onChange={(e)=> setUsername(e.target.value)}
+        value={username}
+      />
+      <input
+        type="text"
+        placeholder="Enter room name"
+        onChange={(e)=> setRoom(e.target.value)}
+        value={room}
+      />
+      <button onClick={join}>Join</button>
     </div>
   );
 }
